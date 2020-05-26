51628ef8dd51c64ed375cc5a5ea97cd9cc6c2626
Hayate Nakano <nakanodesu@gmail.com>
2020-05-26 09:02:28 +0900

    type and macro renaming

--- src/WolframLinearAlgebra.jl
+++ src/WolframLinearAlgebra.jl
@@ -6,66 +6,67 @@ import Base: log, log2, log10, exp
 
 import MathLink: WSymbol, WExpr, WInteger, WReal, weval, parseexpr, @W_str
 
-export WElements, WOperator, @WE_str, @WE_cmd, @WO_str
+export WElem, WSolver, @WE_str, @WE_cmd, @WS_str
 export weval, simplify
 export weigen, wsvd, wqr
 
 _iswlist(x) = false
 _iswlist(x::WExpr) = x.head == W"List"
-struct WElements
+struct WElem
     val::T where {T<:Union{Integer,AbstractFloat,WSymbol,WExpr,WInteger,WReal}}
-    WElements(val) = _iswlist(val) ? error("") : new(val)
+    WElem(val) = _iswlist(val) ? error("") : new(val)
 end
-promote_rule(::Type{WElements}, ::Type{T}) where T<: Number = WElements
+promote_rule(::Type{WElem}, ::Type{T}) where {T<:Number} = WElem
 macro WE_str(s::AbstractString)
-    WElements(WSymbol(s))
+    WElem(WSymbol(s))
 end
 macro WE_cmd(c::Cmd)
-    WElements(parseexpr(c))
+    WElem(parseexpr(c))
 end
 
-function (s::WElements)(args...)
+function (s::WElem)(args...)
     s.val isa WSymbol || error("")
-    all(x -> x isa Union{WElements,Number,WSymbol,WExpr,WInteger,WReal}, args) || error("")
-    any(x -> x isa AbstractIrrational, args) && error("")
-    args = map(x -> x isa WElements ? x : WElements(x), args)
-    WElements((s.val)(map(x -> x.val, args)...))
-end
-(+)(a::WElements, b::WElements) = WE"Plus"(a, b)
-(-)(a::WElements, b::WElements) = WE"Subtract"(a, b)
-(*)(a::WElements, b::WElements) = WE"Times"(a, b)
-(/)(a::WElements, b::WElements) = WE"Divide"(a, b)
-(^)(a::WElements, b::WElements) = WE"Power"(a, b)
-
-(+)(a::WElements, b::Number) = WE"Plus"(a, WElements(b))
-(-)(a::WElements, b::Number) = WE"Subtract"(a, WElements(b))
-(*)(a::WElements, b::Number) = WE"Times"(a, WElements(b))
-(/)(a::WElements, b::Number) = WE"Divide"(a, WElements(b))
-(^)(a::WElements, b::Number) = WE"Power"(a, WElements(b))
-
-(+)(a::Number, b::WElements) = WE"Plus"(WElements(a), b)
-(-)(a::Number, b::WElements) = WE"Subtract"(WElements(a), b)
-(*)(a::Number, b::WElements) = WE"Times"(WElements(a), b)
-(/)(a::Number, b::WElements) = WE"Divide"(WElements(a), b)
-(^)(a::Number, b::WElements) = WE"Power"(WElements(a), b)
-
-WElements(val::Complex) = WElements(real(val)) + WElements(imag(val)) * WE"I"
-WElements(val::Rational) = WElements(numerator(val)) / WElements(denominator(val))
-
-zero(::Type{WElements}) = WElements(0)
-one(::Type{WElements}) = WElements(1)
-
-weval(x::WElements) = WElements(weval(x.val))
-simplify(x::WElements) = weval(WE"Simplify"(x))
-
-struct WOperator
-    op::WSymbol
-end
-macro WO_str(s::AbstractString)
-    WOperator(WSymbol(s))
+    all(x -> x isa Union{WElem,Number,WSymbol,WExpr,WInteger,WReal}, args) || error("")
+    any(x -> x isa AbstractIrrational, args) && error("Irrational is unsupported")
+    args = map(x -> x isa WElem ? x : WElem(x), args)
+    WElem((s.val)(map(x -> x.val, args)...))
 end
 
-function _wjaggedarray(x::AbstractArray{WElements,N}) where {N}
+(+)(a::WElem, b::WElem) = WE"Plus"(a, b)
+(-)(a::WElem, b::WElem) = WE"Subtract"(a, b)
+(*)(a::WElem, b::WElem) = WE"Times"(a, b)
+(/)(a::WElem, b::WElem) = WE"Divide"(a, b)
+(^)(a::WElem, b::WElem) = WE"Power"(a, b)
+
+(+)(a::WElem, b::Number) = a + WElem(b)
+(-)(a::WElem, b::Number) = a - WElem(b)
+(*)(a::WElem, b::Number) = a * WElem(b)
+(/)(a::WElem, b::Number) = a / WElem(b)
+(^)(a::WElem, b::Number) = a ^ WElem(b)
+
+(+)(a::Number, b::WElem) = WElem(a) + b
+(-)(a::Number, b::WElem) = WElem(a) - b
+(*)(a::Number, b::WElem) = WElem(a) * b
+(/)(a::Number, b::WElem) = WElem(a) / b
+(^)(a::Number, b::WElem) = WElem(a) ^ b
+
+WElem(val::Complex) = WElem(real(val)) + WElem(imag(val)) * WE"I"
+WElem(val::Rational) = WElem(numerator(val)) / WElem(denominator(val))
+
+zero(::Type{WElem}) = WElem(0)
+one(::Type{WElem}) = WElem(1)
+
+weval(x::WElem) = WElem(weval(x.val))
+simplify(x::WElem) = weval(WE"Simplify"(x))
+
+struct WSolver
+    solver::WSymbol
+end
+macro WS_str(s::AbstractString)
+    WSolver(WSymbol(s))
+end
+
+function _wjaggedarray(x::AbstractArray{WElem,N}) where {N}
     if N == 1
         map(y -> y.val, x)
     else
@@ -80,11 +81,11 @@ function _parsearray(x::WExpr)
         x = _parsearray.(x.args)
         cat(x...; dims = ndims(first(x)) + 1)
     else
-        WElements.(x.args)
+        WElem.(x.args)
     end
 end
-function (s::WOperator)(a::AbstractMatrix{WElements}, rnum::Int = 1)
-    r = weval((s.op)(_wjaggedarray(a)))
+function (s::WSolver)(a::AbstractMatrix{WElem}, rnum::Int = 1)
+    r = weval((s.solver)(_wjaggedarray(a)))
     if rnum == 1
         _iswlist(r) ? _parsearray(r) : r
     else
@@ -96,37 +97,37 @@ end
 
 # -----
 
-sqrt(a::WElements) = WE"Sqrt"(a)
+sqrt(a::WElem) = WE"Sqrt"(a)
 
-sin(a::WElements) = WE"Sin"(a)
-cos(a::WElements) = WE"Cos"(a)
-tan(a::WElements) = WE"Tan"(a)
+sin(a::WElem) = WE"Sin"(a)
+cos(a::WElem) = WE"Cos"(a)
+tan(a::WElem) = WE"Tan"(a)
 
-sinh(a::WElements) = WE"Sinh"(a)
-cosh(a::WElements) = WE"Cosh"(a)
-tanh(a::WElements) = WE"Tanh"(a)
+sinh(a::WElem) = WE"Sinh"(a)
+cosh(a::WElem) = WE"Cosh"(a)
+tanh(a::WElem) = WE"Tanh"(a)
 
-asin(a::WElements) = WE"ArcSin"(a)
-acos(a::WElements) = WE"ArcCos"(a)
-atan(a::WElements) = WE"ArcTan"(a)
+asin(a::WElem) = WE"ArcSin"(a)
+acos(a::WElem) = WE"ArcCos"(a)
+atan(a::WElem) = WE"ArcTan"(a)
 
-asinh(a::WElements) = WE"ArcSinh"(a)
-acosh(a::WElements) = WE"ArcCosh"(a)
-atanh(a::WElements) = WE"ArcTanh"(a)
+asinh(a::WElem) = WE"ArcSinh"(a)
+acosh(a::WElem) = WE"ArcCosh"(a)
+atanh(a::WElem) = WE"ArcTanh"(a)
 
-log(a::WElements) = WE"Log"(a)
-log2(a::WElements) = WE"Log2"(a)
-log10(a::WElements) = WE"Log10"(a)
+log(a::WElem) = WE"Log"(a)
+log2(a::WElem) = WE"Log2"(a)
+log10(a::WElem) = WE"Log10"(a)
 
-exp(a::WElements) = WE"Exp"(a)
+exp(a::WElem) = WE"Exp"(a)
 
-conj(a::WElements) = WE"Conjugate"(a)
-adjoint(a::WElements) = WE"Conjugate"(a)
+conj(a::WElem) = WE"Conjugate"(a)
+adjoint(a::WElem) = WE"Conjugate"(a)
 
 # -----
 
-weigen(x::AbstractMatrix{WElements}) = WO"Eigensystem"(x, 2)
-wsvd(x::AbstractMatrix{WElements}) = WO"SingularValueDecomposition"(x, 3)
-wqr(x::AbstractMatrix{WElements}) = WO"QRDecomposition"(x, 2)
+weigen(x::AbstractMatrix{WElem}) = WS"Eigensystem"(x, 2)
+wsvd(x::AbstractMatrix{WElem}) = WS"SingularValueDecomposition"(x, 3)
+wqr(x::AbstractMatrix{WElem}) = WS"QRDecomposition"(x, 2)
 
 end # module
